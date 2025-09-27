60796aeade6ac7062f5354c4eaa7f3f268a5c9e4
Jantunen Paula <paula.jantunen@edu.espoo.fi>
2025-09-27 22:46:59 +0300

    d

--- bears-of-sacrifice/player.gd
+++ bears-of-sacrifice/player.gd
@@ -1,49 +1,45 @@
 extends CharacterBody2D
 
-# --- VARIABLES ---
 @export var speed: int = 200
 @export var jump_force: int = -400
 @export var gravity: int = 800
 
 var is_alive: bool = true
-var is_active: bool = false
+var is_active: bool = false   # for multiple characters if needed
 
-# --- PHYSICS ---
 func _physics_process(delta):
-	if not is_alive or not is_active:
+	if not is_alive:
+		return
+	if not is_active:
 		velocity = Vector2.ZERO
 		return
 
-	var input_dir = Input.get_axis("move_left", "move_right")
-	velocity.x = input_dir * speed
-
+	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	else:
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = jump_force
-			$JumpSound.play()
+
+	# Horizontal movement
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+		$JumpSound.play()
 
 	move_and_slide()
 
-# --- INPUT ---
-func _input(event):
-	if event.is_action_pressed("sacrifice") and is_alive and is_active:
+	# Sacrifice action
+	if Input.is_action_just_pressed("sacrifice"):
 		do_sacrifice()
 
-# --- SACRIFICE ---
+# Play sacrifice animation
 func do_sacrifice():
-	$AnimationPlayer.play("SacrificeFade")  # fade animation calls do_final_sacrifice() at end
+	$AnimationPlayer.play("SacrificeFade")
 
+# Called at the end of fade animation
 func do_final_sacrifice():
-	# Play sound
 	$SacrificeSound.play()
-
-	# Spawn particle effect
-	var effect_scene = load("res://BridgeEffect.tscn")
-	var effect = effect_scene.instantiate()
-	effect.global_position = global_position
-	get_parent().add_child(effect)
 
 	# Spawn bridge block
 	var bridge_scene = load("res://BridgeBlock.tscn")
@@ -51,6 +47,5 @@ func do_final_sacrifice():
 	bridge.global_position = $SacrificePosition.global_position
 	get_parent().add_child(bridge)
 
-	# Remove player
 	is_alive = false
 	queue_free()
